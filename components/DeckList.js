ee2fae66598721dec933903d2117dade31f18afb
Shannon Rivers <shannon.rivers@gmail.com>
2018-01-29 12:49:00 -0700

    working app with one component

---
+++ components/DeckList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Stylesheet, View, Text} from 'react-native';
+
+export default class DeckList extends React.Component {
+  render () {
+    return (
+      <View>
+        <Text>
+          Let's learn some stuff!!
+        </Text>
+      </View>
+    )
+  };
+};
