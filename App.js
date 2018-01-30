5c36448efe2e069e0f22bfbd0784ac58373d4390
Shannon Rivers <shannon.rivers@gmail.com>
2018-01-30 13:22:26 -0700

    add TabNavigator not rendering

--- App.js
+++ App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {View, Text} from 'react-native';
 import DeckList from './components/DeckList';
+import AddDeck from './components/AddDeck';
 import {TabNavigator} from 'react-navigation';
-import {FontAwesome, MaterialCommunityIcons} from '@expo-vector-icons';
-import {white, orange} from '../utils/colors';
+import {FontAwesome, MaterialCommunityIcons} from '@expo/vector-icons';
+import {white, orange, redOrange} from './utils/colors';
+
+const Tabs = TabNavigator({
+  DeckList: {
+    screen: DeckList,
+    navigationOptions: {
+      tabBarLabel: 'Decks',
+      tabBarIcon: ({tintColor})=> <MaterialCommunityIcons name='cards' size={30} color={tintColor}/>
+    }
+  },
+  AddDeck: {
+    screen: AddDeck,
+    navigationOptions: {
+      tabBarLabel: 'Add a Deck',
+      tabBarIcon: ({tintColor})=><FontAwesome name='plus-square' size={30} color={tintColor}/>
+    }
+  }
+}, {
+  tabBarOptions: {
+    activeTintColor: redOrange,
+    style: {
+      height: 56,
+      backgroundColor: white
+    }
+  }
+});
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <DeckList />
+      <View style={{flex:1}}>
+        <Tabs/>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: orange,
-    color: white,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
