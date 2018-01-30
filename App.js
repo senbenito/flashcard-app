ba161b0298fd95afb3eed42642f81057fcaab086
Shannon Rivers <shannon.rivers@gmail.com>
2018-01-30 12:38:24 -0700

    add navigation, icons and AddDeck component stubs

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import DeckList from './components/DeckList';
+import {TabNavigator} from 'react-navigation';
+import {FontAwesome, MaterialCommunityIcons} from '@expo-vector-icons';
+import {white, orange} from '../utils/colors';
 
 export default class App extends React.Component {
   render() {
@@ -15,7 +18,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: orange,
+    color: white,
     alignItems: 'center',
     justifyContent: 'center',
   },
